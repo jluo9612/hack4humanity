be622ad4b854efe70ee08d61385626aff1778eed
louiseruiwenli <louiseruiwenli@gmail.com>
2017-03-07 14:44:35 -0800

    --branch

---
+++ php/28.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Description</title>
+	<link rel="stylesheet" href="bootstrap.css">
+
+<link rel="stylesheet" href="description.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h1 id="title">Title:<?php  echo PPPPP; ?></h1>
+			<p id="organization">Organization:<?php echo SCU; ?></p>
+			<p>Program Size:<?php echo small; ?> </p>
+			<p><?php echo 200 casa; ?></p>
+			<p>Program Description:<?php echo xxx; ?></p>
+			<p>Skill reconmended:<?php echo xxx; ?></p>
+			<p>requirement:<?php echo xxx; ?></p>
+			<p>	</p>
+		</div>
+	</div>
+</body>
+</html>
